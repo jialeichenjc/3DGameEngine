58b5de943b2342f19bb5adcec42f47566b96aa98
joycechendev <superbrainchen@gmail.com>
2016-09-06 15:49:31 -0600

    Modified gitignore To untrack all unnecessary files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <conio.h>
 #include "Monster.h"
 #include "Player.h"
@@ -11,13 +12,13 @@ int main() {
 	scanf_s("%d", &num_monsters);
 	//constexpr int num_monsters = num_monsters_input;
 	Monster *monsters = new Monster[num_monsters];
-	char *test_str = "";
 	for (int i = 0; i < num_monsters; i++) {
 		Monster monster;
 		char monster_name[30];
 		printf("Please enter your name for monster number %d: ", i);
-		scanf_s("%s", test_str);
+		scanf_s("%s", monster_name, sizeof(monster_name));
 		monster.set_name(monster_name);
+		printf("monster %s is at position [%d, %d]\n", monster.get_name(), monster.position[0], monster.position[1]);
 	}
 	printf("\n");
 	//cout << num_monsters;
