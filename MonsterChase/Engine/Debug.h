f8aaa1701eaac86af2a1a7299e4aa7ff200f9566
joycechendev <superbrainchen@gmail.com>
2016-10-04 21:15:52 -0600

    Clean up repo

---
+++ MonsterChase/Engine/Debug.h
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+#ifndef __DEBUG_H
+#define __DEBUG_H
+#define EMIT_LOCATION() printf("Line: %d in File: %s", __FILE__, __LINE__);
+
+#define DEBUG_PRINT(fmt,...) printf(fmt, __VA_ARGS__)
+
+#if defined(_MSC_VER)
+#define BREAK_IN_DEBUGGER() __debugbreak()
+
+#elif defined(__GNUC__)
+#define BREAK_IN_DEBUGGER() __builtin_trap()
+#else
+#error "Provide an implementation of BREAK_IN_DEBUGGER"
+#endif
+
+#if defined(_DEBUG)
+#define assert(cond, error_str)	if(cond == false) {\
+						DEBUG_PRINT("%s occurred at File %s Line %d", error_str, __FILE__, __LINE__);\
+						BREAK_IN_DEBUGGER();}
+						
+						
+#else
+#define assert(cond) void(0)
+#endif
+#endif
+
