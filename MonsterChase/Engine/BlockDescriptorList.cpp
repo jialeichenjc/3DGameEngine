c8ad142e292cb1550fbbb4b186774a2cc4798be7
joycechendev <superbrainchen@gmail.com>
2016-10-28 22:22:48 -0600

    Remove commented lines

--- MonsterChase/Engine/BlockDescriptorList.cpp
+++ MonsterChase/Engine/BlockDescriptorList.cpp
@@ -38,43 +38,10 @@ void BlockDescriptorList::insert_after(BlockDescriptor *pos, BlockDescriptor &bd
 }
 
 void BlockDescriptorList::remove(BlockDescriptor *pos) {
-	//BlockDescriptor bd = *pos;
-	//BlockDescriptor *pos_next = pos->next_bd;
-	//BlockDescriptor *pos_prev = pos->prev_bd;
 	if (head == NULL) {
 		return;
 	}
 	else {
-		/*
-		BlockDescriptor *curr = head;
-		while (curr != NULL) {
-			if (curr->block_base_ptr == pos->block_base_ptr) {
-				if (curr->prev_bd == NULL && curr->next_bd == NULL) {
-					head = NULL;
-					tail = NULL;
-				}
-				else if (curr->prev_bd != NULL && curr->next_bd == NULL) {
-					// set the new tail
-					tail = curr->prev_bd;
-					curr->prev_bd->next_bd = NULL;
-					curr->prev_bd = NULL;
-				}
-				else if (curr->prev_bd == NULL && curr->next_bd != NULL) {
-					head = curr->next_bd;
-					curr->next_bd->prev_bd = NULL;
-					curr->next_bd = NULL;
-				}
-				else if (curr->prev_bd != NULL && curr->next_bd != NULL) {
-					curr->prev_bd->next_bd = curr->next_bd;
-					curr->next_bd->prev_bd = curr->prev_bd;
-					curr->next_bd = NULL;
-					curr->next_bd = NULL;
-				}
-				break;
-			}
-			curr = curr->next_bd;
-		}
-		*/
 		
 		if (pos->prev_bd == NULL && pos->next_bd == NULL) {
 			head = NULL;
@@ -99,50 +66,7 @@ void BlockDescriptorList::remove(BlockDescriptor *pos) {
 		}
 		size--;
 	}
-	/*
-	if (pos->next_bd == NULL && pos->prev_bd == NULL) {
-		return;
-	}
-	else if (pos->next_bd == NULL && pos->prev_bd != NULL) {
-		pos->prev_bd->next_bd = NULL;
-		pos->prev_bd = NULL;
-	}
-	else if (pos->prev_bd == NULL && pos->next_bd != NULL) {
-		pos->next_bd->prev_bd = NULL;
-		pos->next_bd = NULL;
-	}
-	else {
-		pos->prev_bd->next_bd = pos->next_bd;
-		pos->next_bd->prev_bd = pos->prev_bd;
-		pos->next_bd = NULL;
-		pos->prev_bd = NULL;
-	}
-	*/
 }
-
-/*
-void BlockDescriptorList::remove(BlockDescriptor &pos) {
-	//BlockDescriptor *pos_next = pos->next_bd;
-	//BlockDescriptor *pos_prev = pos->prev_bd;
-	if (pos->next_bd == NULL && pos->prev_bd == NULL) {
-		return;
-	}
-	else if (pos->next_bd == NULL && pos->prev_bd != NULL) {
-		pos->prev_bd->next_bd = NULL;
-		pos->prev_bd = NULL;
-	}
-	else if (pos->prev_bd == NULL && pos->next_bd != NULL) {
-		pos->next_bd->prev_bd = NULL;
-		pos->next_bd = NULL;
-	}
-	else {
-		pos->prev_bd->next_bd = pos->next_bd;
-		pos->next_bd->prev_bd = pos->prev_bd;
-		pos->next_bd = NULL;
-		pos->prev_bd = NULL;
-	}
-}
-*/
 
 
 // add to the end of list
@@ -164,25 +88,6 @@ void BlockDescriptorList::push(BlockDescriptor *bd) {
 	size++;
 }
 
-/*
-void BlockDescriptorList::push(size_t size) {
-	BlockDescriptor bd;
-	bd.block_size = size;
-	BlockDescriptor *p_bd = &bd;
-	if (head == NULL) {
-		head = p_bd;
-	}
-	else if (head != NULL && tail == NULL) {
-		tail = p_bd;
-		head->next_bd = tail;
-		tail->prev_bd = head;
-	}
-	else {
-		tail->next_bd = p_bd;
-		p_bd->prev_bd = tail;
-	}
-}
-*/
 void BlockDescriptorList::print_block_list() {
 	BlockDescriptor *curr = head;
 	while (curr != NULL) {
