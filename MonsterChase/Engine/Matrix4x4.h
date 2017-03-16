d7a96e378ebb7d360a7c9a7a7de821d3cf46a979
Joyce Chen <superbrainchen@gmail.com>
2017-03-16 15:45:11 -0600

    More Matrix stuff

--- MonsterChase/Engine/Matrix4x4.h
+++ MonsterChase/Engine/Matrix4x4.h
@@ -5,6 +5,12 @@ class Matrix4x4
 public:
 	Matrix4x4();
 	~Matrix4x4();
+
+private:
+	float m_11, m_12, m_13, m_14,
+		m_21, m_22, m_23, m_24,
+		m_31, m_32, m_33, m_34,
+		m_41, m_42, m_43, m_44;
 };
 
-inline 
+
