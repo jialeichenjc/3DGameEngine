09b605df22d91ec3197cd6f2a6e732e2d52b62eb
Joyce Chen <superbrainchen@gmail.com>
2017-04-10 16:42:44 -0600

    Add unsaved files for last commit

--- MonsterChase/Engine/MessageSystem.h
+++ MonsterChase/Engine/MessageSystem.h
@@ -3,14 +3,16 @@
 #include "IActorType.h"
 namespace MessageSystem {
 	typedef void(*MessageHandler_t)(const HashedString & i_Message);
-
-	void RegisterMessageHandler(const HashedString & i_Message, MessageHandler_t i_pHandler);
-
 	class IMessageHandler {
 	public:
 		virtual void HandleMessage(const HashedString & i_Message, IActorType * i_pDetails) = 0;
 		~IMessageHandler() {}
 	};
+
+	void RegisterMessageHandler(const HashedString & i_Message, MessageHandler_t i_pHandler);
+	void DeregisterMessageHandler(const HashedString & i_Message, IMessageHandler * i_pHandler);
+
+	void SendMessage(const HashedString & i_Message, void * i_pMessageDetails);
 }
 
 //class MessageSystem
